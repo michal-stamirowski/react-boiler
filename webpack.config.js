f2962181bf8c0a6711e1c142c3473c69562937c2
Malhavoc <michal.stamirowski@gmail.com>
2018-07-15 23:19:28 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+import HtmlWebPackPlugin from 'html-webpack-plugin';
+import nodeExternals from 'webpack-node-externals';
+import path from 'path';
+
+const moduleObj = {
+    loaders: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loaders: ["babel-loader"],
+        }
+    ],
+};
+const client = {
+  entry: {
+    'client': './src/client/index.js'
+  },
+  target: 'web',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist/public')
+  },
+  module: moduleObj,
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: 'src/client/index.html'
+    })
+  ]
+}
+const server = {
+    entry: {
+        'server': './src/server/index.js'
+    },
+    target: 'node',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: moduleObj,
+    externals: [nodeExternals()]
+}
+module.exports = [client, server];
